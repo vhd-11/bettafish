4e39f7d9698e73002d9bea769e79b62f2022ce50
pterodactylscreams <pterodactylscreams@gmail.com>
2025-03-30 13:29:14 +0530

    login to do

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -3,27 +3,37 @@ import { Link } from 'react-router-dom'
 import { Button } from '@/components/ui/button'
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
 import companies from "../data/companies.json"
+import Autoplay from 'embla-carousel-autoplay'
+import faq from "../data/faq.json"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
 
 const LandingPage = () => {
     return (
-        <main className='flex flex-col gap-10 sm:gap-20 py-10 sm:py-20 text-white/85'>
+        <main className='flex flex-col gap-10 sm:gap-20 py-15 sm:py-20 text-white/85'>
             <section className='text-center'>
                 <p className=' flex flex-col items-center text-6xl font-dark text-white mb-3 sm:text-7xl lg:text-8xl'>
                     Reinovating the
                     <span className='italic'>Job Hunt</span>
                 </p>
-                <p className='sm:mt-4 text- sm:text-xl font-medium'>
+                <p className='sm:mt-4 text-white sm:text-xl font-light'>
                     Fish for both jobs and candidates
                     with <span className='text-pink-400 font-semibold'>Bettafish</span></p>
             </section>
+
+            {/* buttons */}
             <div className='flex justify-center items-start gap-8'>
                 <Link to="/jobs">
                     <div>
-                        <a href="#_" class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-pink-400 transition duration-300 ease-out border-2 border-pink-400 rounded-full shadow-md group">
-                            <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-pink-400 group-hover:translate-x-0 ease">
+                        <a href="#_" class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-pink-300 rounded-full shadow-md group bg-pink-400">
+                            <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-220 -translate-x-full bg-pink-400 group-hover:translate-x-0 ease">
                                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                             </span>
-                            <span class="absolute flex items-center justify-center w-full h-full text-pink-00 transition-all duration-300 transform group-hover:translate-x-full ease">Candidate</span>
+                            <span class="absolute flex items-center justify-center w-full h-full text-pink-00 transition-all duration-400 transform group-hover:translate-x-full ease bg-pink-400">Candidate</span>
                             <span class="relative invisible">Candidate</span>
                         </a>
                     </div>
@@ -31,34 +41,54 @@ const LandingPage = () => {
 
                 <Link to="/post-job">
                     <div>
-                        <a href="#_" class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-pink-400 transition duration-300 ease-out border-2 border-pink-400 rounded-full shadow-md group">
-                            <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-pink-400 group-hover:translate-x-0 ease">
+                        <a href="#_" class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-pink-300 rounded-full shadow-md group bg-pink-400">
+                            <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-220 -translate-x-full bg-pink-400 group-hover:translate-x-0 ease">
                                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                             </span>
-                            <span class="absolute flex items-center justify-center w-full h-full text-pink-00 transition-all duration-300 transform group-hover:translate-x-full ease">Employer</span>
+                            <span class="absolute flex items-center justify-center w-full h-full text-pink-00 transition-all duration-300 transform group-hover:translate-x-full ease bg-pink-400">Employer</span>
                             <span class="relative invisible">Employer</span>
                         </a>
                     </div>
                 </Link>
             </div>
 
-                {/* carousel */}
-                {/* cards */}
-                <Carousel className="w-full py-10">
-                    <CarouselContent className='flex gap-10 sm:gap-20 items-center'>
-                        {companies.map(({ name, id, path }) => {
+            {/* carousel */}
+            <Carousel plugins={[
+                Autoplay({
+                    delay: 1500,
+                }),
+            ]}
+                className="w-full py-8 px-6">
+                <CarouselContent className='flex gap-10 sm:gap-20 items-center'>
+                    {companies.map(({ name, id, path }) => {
 
-                            return (
-                                <CarouselItem key = {id} className='basis-1/3 lg:basis-1/6'>
-                                    <img src={path} alt={name} className='h-9 sm:h-14 object-contain' />
-                                </CarouselItem>
-                            );
-                        })}
-                    </CarouselContent>
-                </Carousel>
+                        return (
+                            <CarouselItem key={id} className='basis-1/3 lg:basis-1/6'>
+                                <img src={path} alt={name} className='h-9 sm:h-14 object-contain' />
+                            </CarouselItem>
+                        );
+                    })}
+                </CarouselContent>
+            </Carousel>
 
             {/* accordion */}
-        </main>
+            {/* <div className='px-6 sm:px-12'>Frequently Asked Questions</div> */}
+            <Accordion className='py-7 px-6 mx-10 sm:mx-13 sm:px-13 bg-rose-300/35 shadow-2xl' type="single" collapsible>
+                {faq.map((faqIt, index) => {
+                    return (
+                    <AccordionItem className='text-white' key={index} value={`item-${index + 1}`}>
+                        <AccordionTrigger className='sm:text-base'>{faqIt.question}</AccordionTrigger>
+                        <AccordionContent>
+                            {faqIt.answer}
+                        </AccordionContent>
+                    </AccordionItem>
+                    );
+                })
+                }
+
+            </Accordion>
+
+        </main >
     )
 }
 
