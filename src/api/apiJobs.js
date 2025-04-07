10ead31da9b82b7c80be28218768301fa9f79533
pterodactylscreams <pterodactylscreams@gmail.com>
2025-04-08 00:30:44 +0530

    create logic for fetching details of a job

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -61,3 +61,21 @@ export async function saveJob(token, { alreadySaved }, saveData) {
 
     }
 }
+
+export async function getSingleJob(token, {job_id}) {
+    const supabase = await supabaseClient(token);
+
+
+    const { data, error } = await supabase
+        .from("jobs")
+        .select("*, company: companies(name, logo_url), applications: applications(*)")
+        .eq("id", job_id)
+        .single();
+
+    if (error) {
+        console.error("Error fetching company: ", error);
+        return null;
+    }
+
+    return data;
+}
