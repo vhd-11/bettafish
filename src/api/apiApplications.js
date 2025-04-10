67034e96896a71a1c879aa9b872fb89f3d7a93c0
pterodactylscreams <pterodactylscreams@gmail.com>
2025-04-10 19:17:17 +0530

    set api file for job application

--- src/api/apiApplications.js
+++ src/api/apiApplications.js
@@ -1,14 +1,32 @@
-import supabaseClient from "@/utils/supabase";
+import supabaseClient, { supabaseUrl } from "@/utils/supabase";
 export async function applyToJob(token, _, jobData) {
     const supabase = await supabaseClient(token);
 
 
+    const random = Math.floor(Math.random()*9000);
+    const fileName = `resume-${random}-${jobData.candidate_id}`
+
+    const {error:storageError} = await supabase.storage.from('resumes').upload(fileName, jobData.resume)
+
+    if (storageError){
+        console.error("Error uploading resume: ", error)
+        return null;
+    }
+
+    const resume = `${supabaseUrl}/storage/v1/object/public/resumes/${fileName}`
+
     const { data, error } = await supabase
-        .from("companies")
-        .select("*")
+        .from("applications")
+        .insert([
+            {
+                ...jobData,
+                resume,
+            },
+        ])
+        .select();
 
     if (error) {
-        console.error("Error deleting saved jobs: ", error);
+        console.error("Error submitting application ", error);
         return null;
     }
 
