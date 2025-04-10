1d544b1380bcabb205319a6d31ada23dff646325
pterodactylscreams <pterodactylscreams@gmail.com>
2025-04-10 16:06:49 +0530

    render job desc  n requirements

--- src/pages/job.jsx
+++ src/pages/job.jsx
@@ -1,7 +1,7 @@
 import { getSingleJob } from '@/api/apiJobs'
 import useFetch from '@/hooks/use-fetch'
 import { useUser } from '@clerk/clerk-react'
-import { BriefcaseBusinessIcon, Clock10Icon, MapPinIcon } from 'lucide-react'
+import { BriefcaseBusinessIcon, Clock10Icon, DoorClosedIcon, DoorOpenIcon, MapPinIcon, PersonStandingIcon } from 'lucide-react'
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { ClipLoader } from 'react-spinners'
@@ -34,31 +34,79 @@ const JobPage = () => {
 
     return (
         <div>
+
+            {/* big container getting all of this HEADER CONTAINER */}
             <div className='m-10'>
+
+                {/* logo */}
                 <div className='flex justify-start sm:gap-2.5 lg:gap-3 items-start gap-2'>
-                    <img src={job?.company?.logo_url} className='bg-slate-100 rounded-xl p-2 h-14 lg:h-15' alt={job?.title} />
+                    <img src={job?.company?.logo_url} className='bg-slate-100/70 rounded-xl p-2 h-15 lg:h-18' alt={job?.title} />
+
+                    {/* job title n company name */}
                     <div className='flex flex-col'>
                         <div className='flex flex-col'>
-                            <h1 className='sm:text-2xl lg:text-4xl text-xl'>{job?.title}</h1>
+                            <h1 className='lg:text-5xl text-3xl font-medium'>{job?.title}</h1>
+                            {/* num of applicants */}
+
+
                             <p className='lg:text-lg text-pink-400 font-bold tracking-wider'>{job?.company?.name}</p>
                         </div>
-                        <div className='flex gap-4'>
-                            <div className='flex gap-1'>
-                                <MapPinIcon></MapPinIcon>
-                                <div>{job?.location}</div>
+
+                        {/* 3 tabs for fulltime remote n location */}
+                        <div className='flex text-sm mt-3 text-gray-500 font-medium flex-col gap-2 lg:gap-4'>
+                            <div className='flex gap-2'>
+                                <div className='flex gap-0.2 items-baseline bg-slate-100/70 p-2 rounded-xl'>
+                                    <MapPinIcon height={'14'}></MapPinIcon>
+                                    <div>{job?.location}</div>
+                                </div>
+                                <div className='flex gap-0.2 items-center bg-slate-100/70 p-2 rounded-xl'>
+                                    <BriefcaseBusinessIcon height={'14'} ></BriefcaseBusinessIcon>
+                                    <div>{job?.mode}</div>
+                                </div>
+                                <div className='flex gap-0.2 items-center bg-slate-100/70 p-2 rounded-xl'>
+                                    <Clock10Icon height={'14'}></Clock10Icon>
+                                    <div>{job?.whattime}</div>
+                                </div>
                             </div>
-                            <div className='flex gap-1'>
-                                <BriefcaseBusinessIcon></BriefcaseBusinessIcon>
-                                <div>{job?.mode}</div>
-                            </div>
-                            <div className='flex gap-1'>
-                                <Clock10Icon></Clock10Icon>
-                                <div>{job?.whattime}</div>
+
+                            <div className='flex justify-start'>
+                                <div className='flex gap-0.2 items-baseline p-2 rounded-xl'>
+                                    <PersonStandingIcon height={'14'}></PersonStandingIcon>
+                                    <div>{`${job?.applications?.length} applicants`}</div>
+                                </div>
+
+                                {/* close/open application */}
+                                <div className='flex flex-row gap-6'>
+                                    <div className='flex gap-0.2 items-center p-2 rounded-xl'>
+                                        {job?.isOpen ? (
+                                            <>
+                                                <DoorOpenIcon color='#008236' height={'16'} /> <p className='text-green-700'>Open</p>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <DoorClosedIcon color='#C10007' />  <p className='text-red-700'>Closed</p>
+                                            </>
+                                        )}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <hr className='ml-10 mr-10'></hr>
+
+            {/* TODO:hiring status */}
+
+            {/* job deets */}
+            <div className='lg:m-10 ml-10 mt-5' >
+                <p className='lg:text-3xl text-xl font-medium'>Job Details</p>
+                <p className='mt-2 lg:mt-5 text-gray-500 font-normal'>{job?.description}</p>
+
+                <p className='lg:text-3xl text-xl font-medium lg:mt-12 mt-8'>Requirements</p>
+                <p className='mt-2 lg:mt-5 text-gray-500 font-normal'>{job?.requirements}</p>
+            </div>
+            
         </div>
     )
 }
