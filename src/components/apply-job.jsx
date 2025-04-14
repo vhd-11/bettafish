20119b5b6b9d37a74393474b99773650e66b18aa
pterodactylscreams <pterodactylscreams@gmail.com>
2025-04-14 13:05:48 +0530

    change diemsnions of job apply small on big screens

--- src/components/apply-job.jsx
+++ src/components/apply-job.jsx
@@ -20,20 +20,18 @@ const ApplyJob = ({ user, job, applied = false, fetchJob }) => {
             <Drawer open={applied ? false : undefined}>
             <div className="w-full flex justify-center">
                 <DrawerTrigger >
-
-
                         <Button
-                            variant={"default"} className={"h-11 lg:h-13 cursor-pointer mt-8 w-sm sm:w-xl md:w-2xl lg:w-4xl xl:w-6xl"}
+                            variant={"default"} className={"h-11 lg:mt-12 cursor-pointer mt-8 w-sm sm:w-xl md:w-2xl"}
                             size="lg"
                             disabled={!job?.isOpen || applied}
                         >
-                            {job?.isOpen ? (applied ? "Applied" : "Apply") : "Hiring closed!"}
+                            {job?.isOpen ? (applied ? "Already applied for this job" : "Apply for this job") : "Hirings are closed for this job!"}
                         </Button>
                 </DrawerTrigger>
                 </div>
                 <DrawerContent>
                     <DrawerHeader>
-                        <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                        <DrawerTitle>Apply for {job?.title} at {job?.company?.name}</DrawerTitle>
                         <DrawerDescription>This action cannot be undone.</DrawerDescription>
                     </DrawerHeader>
                     <DrawerFooter>
